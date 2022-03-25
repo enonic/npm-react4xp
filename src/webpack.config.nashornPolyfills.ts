b43a5ce072417eeab2d3d8bd03f619f2ce215b39
Christian Westgaard <comlock@gmail.com>
2022-03-25 09:51:35 +0100

    Refactor nashornPolyfills to typescript

--- src/webpack.config.nashornPolyfills.ts
+++ src/webpack.config.nashornPolyfills.ts
@@ -1,36 +1,35 @@
 // Transpiles nashorn polyfill from, among other things, npm libraries.
-const {
-  statSync
-} = require("fs");
+import type {Environment} from './index.d';
 
-const {
+
+import {statSync} from 'fs';
+
+import {
   isAbsolute,
   join,
   resolve
-} = require('path');
+} from 'path';
 
-const {
+import {
   DIR_PATH_RELATIVE_BUILD_ASSETS_R4X,
   FILE_NAME_R4X_PROPERTIES
-} = require('../dist/constants.buildtime');
+} from './constants.buildtime';
 
-const {FILE_STEM_NASHORNPOLYFILLS_USERADDED} = require('../dist/constants.runtime');
-const {getProperties} = require("../dist/properties/getProperties");
-const {cleanAnyDoublequotes} = require('../dist/util/cleanAnyDoublequotes');
-const {isSet} = require("../dist/util/isSet");
-const {makeVerboseLogger} = require('../dist/util/makeVerboseLogger');
+import {FILE_STEM_NASHORNPOLYFILLS_USERADDED} from './constants.runtime';
+import {getProperties} from './properties/getProperties';
+import {cleanAnyDoublequotes} from './util/cleanAnyDoublequotes';
+import {isSet} from './util/isSet';
+import {makeVerboseLogger} from './util/makeVerboseLogger';
 
 
-module.exports = (env) => {
-  env = env || {}; // eslint-disable-line no-param-reassign
-
-  const DIR_PATH_ABSOLUTE_PROJECT = cleanAnyDoublequotes("DIR_PATH_ABSOLUTE_PROJECT", env.DIR_PATH_ABSOLUTE_PROJECT || process.cwd());
+module.exports = (env :Environment = {}) => {
+  const DIR_PATH_ABSOLUTE_PROJECT = cleanAnyDoublequotes('DIR_PATH_ABSOLUTE_PROJECT', env.DIR_PATH_ABSOLUTE_PROJECT || process.cwd());
   if (!isAbsolute(DIR_PATH_ABSOLUTE_PROJECT)) {
     throw new Error(`env.DIR_PATH_ABSOLUTE_PROJECT:${DIR_PATH_ABSOLUTE_PROJECT} not an absolute path!`);
   }
 
   const DIR_PATH_ABSOLUTE_BUILD_ASSETS_R4X = join(DIR_PATH_ABSOLUTE_PROJECT, DIR_PATH_RELATIVE_BUILD_ASSETS_R4X);
-  const FILE_PATH_ABSOLUTE_SRC_NASHORNPOLYFILLS_DEFAULT = join(__dirname, 'nashornPolyfills.es6');
+  //const FILE_PATH_ABSOLUTE_SRC_NASHORNPOLYFILLS_DEFAULT = join(__dirname, 'nashornPolyfills.es6');
 
   let {
     BUILD_ENV = 'production',
@@ -96,19 +95,19 @@ module.exports = (env) => {
     				/node_modules[\\/]webpack[\\/]buildin/ // will cause errors if they are transpiled by Babel
     			],
           use: {
-            loader: "babel-loader",
+            loader: 'babel-loader',
             options: {
               babelrc: false,
               comments: false,
-              compact: BUILD_ENV !== "development",
-              minified: BUILD_ENV !== "development",
+              compact: BUILD_ENV !== 'development',
+              minified: BUILD_ENV !== 'development',
               presets: [
-                "@babel/preset-react",
-                "@babel/preset-env"
+                '@babel/preset-react',
+                '@babel/preset-env'
               ],
               plugins: [
-                "@babel/plugin-proposal-object-rest-spread",
-                "@babel/plugin-transform-arrow-functions",
+                '@babel/plugin-proposal-object-rest-spread',
+                '@babel/plugin-transform-arrow-functions',
                 '@babel/plugin-transform-block-scoping' // transpile 'const' and 'let to 'var'
               ],
             },
@@ -118,13 +117,13 @@ module.exports = (env) => {
     }, // module
 
     optimization: {
-  		minimize: BUILD_ENV !== "development"
+  		minimize: BUILD_ENV !== 'development'
   	},
 
     output: {
       path: DIR_PATH_ABSOLUTE_BUILD_ASSETS_R4X,
 
-      filename: "[name].js", // No need for contenthash as this is serverside
+      filename: '[name].js', // No need for contenthash as this is serverside
 
       environment: {
         arrowFunction: false,
@@ -145,13 +144,13 @@ module.exports = (env) => {
       // library or libraryTarget must be set!
 
       //libraryTarget: 'commonjs' // 7963B
-      //libraryTarget: 'commonjs-module' // 7870B // ReferenceError: "module" is not defined
+      //libraryTarget: 'commonjs-module' // 7870B // ReferenceError: 'module' is not defined
       libraryTarget: 'commonjs-static' // 7907B
-      //libraryTarget: 'commonjs2' // 7870B // ReferenceError: "module" is not defined
+      //libraryTarget: 'commonjs2' // 7870B // ReferenceError: 'module' is not defined
 
       /*library: {  // 7893B Trying out what is used in buildComponents, also transpiles 'const' and 'let' to 'var'
-        name: "[name]",
-        type: "var",
+        name: '[name]',
+        type: 'var',
       }*/
 
       // To make UMD build available on both browsers and Node.js, set output.globalObject option to 'this'.
@@ -163,7 +162,7 @@ module.exports = (env) => {
     }, // output
 
     resolve: {
-      extensions: [".es6", ".js", ".jsx"],
+      extensions: ['.es6', '.js', '.jsx'],
       modules: [
         resolve(DIR_PATH_ABSOLUTE_PROJECT, 'node_modules'),
         'node_modules'
