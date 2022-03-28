82647c734afec76e7ad48b005851bf92a603a64f
Christian Westgaard <comlock@gmail.com>
2022-03-28 10:45:29 +0200

    Do not generate runtimeSettings.json #17

--- src/constants.runtime.ts
+++ src/constants.runtime.ts
@@ -3,7 +3,6 @@
 export const COMPONENT_STATS_FILENAME = 'stats.components.json';
 export const ENTRIES_FILENAME = 'entries.json';
 export const EXTERNALS_CHUNKS_FILENAME = 'chunks.externals.json';
-export const FILE_NAME_R4X_RUNTIME_SETTINGS = 'runtimeSettings.json';
 
 export const FILE_STEM_NASHORNPOLYFILLS_USERADDED = 'nashornPolyfills.userAdded';
 
