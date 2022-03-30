9d3dee7ee410be25fcae89bb978c093ae81d4e3d
Christian Westgaard <comlock@gmail.com>
2022-03-30 20:24:59 +0200

    Move all react4xp.properties into react4xp.config.js #24

--- src/constants.buildtime.ts
+++ src/constants.buildtime.ts
@@ -14,7 +14,6 @@ export const EXTERNALS_DEFAULT = {
   'react-dom/server': 'ReactDOMServer',
 };
 
-export const FILE_NAME_R4X_CONFIG_JSON = 'react4xp.config.json';
+export const FILE_NAME_R4X_CONFIG_JS = 'react4xp.config.js';
 export const FILE_NAME_R4X_NASHORN_POLYFILLS = 'react4xpNashornPolyfills.es6';
-export const FILE_NAME_R4X_PROPERTIES = 'react4xp.properties';
 export const FILE_NAME_WEBPACK_CONFIG_R4X_JS = 'webpack.config.react4xp.js';
