3604ad3b84513597d9b18fd5afcbd8d312b30ceb
Christian Westgaard <comlock@gmail.com>
2022-03-31 10:09:55 +0200

    nashornPolyfills.userAdded.js is not an clientside asset and should be moved out of asset/react4xp #32

--- src/constants.runtime.ts
+++ src/constants.runtime.ts
@@ -4,8 +4,6 @@ export const COMPONENT_STATS_FILENAME = 'stats.components.json';
 export const ENTRIES_FILENAME = 'entries.json';
 export const EXTERNALS_CHUNKS_FILENAME = 'chunks.externals.json';
 
-export const FILE_STEM_NASHORNPOLYFILLS_USERADDED = 'nashornPolyfills.userAdded';
-
 export const LIBRARY_NAME = 'React4xp';
 
 export const R4X_TARGETSUBDIR = 'assets/react4xp';
