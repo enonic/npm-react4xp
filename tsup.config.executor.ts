afe0d3f844070b2b59b60699b235b9d6f3fc1871
Christian Westgaard <comlock@gmail.com>
2023-03-20 13:06:10 +0100

    typings

--- tsup.config.executor.ts
+++ tsup.config.executor.ts
@@ -1,25 +1,35 @@
 import manifestPlugin from 'esbuild-plugin-manifest';
 import { isAbsolute, join } from 'path';
 import { print } from 'q-i';
-import { defineConfig } from 'tsup';
+import { defineConfig, type Options } from 'tsup';
 import getAppName from './src/util/getAppName';
 import { LIBRARY_NAME } from './src/constants.runtime';
 
+interface MyOptions extends Options {
+	env?: {
+		APP_NAME?: string
+		DIR_PATH_ABSOLUTE_PROJECT?: string
+		LIBRARY_NAME?: string
+	}
+}
 
-export default defineConfig((options) => {
+export default defineConfig((options: MyOptions) => {
 	// print(options, { maxItems: Infinity });
 	const {
 		env: {
 			DIR_PATH_ABSOLUTE_PROJECT
-		}
+		} = {}
 	} = options;
-	if (!isAbsolute(DIR_PATH_ABSOLUTE_PROJECT)) {
+	if (!DIR_PATH_ABSOLUTE_PROJECT || !isAbsolute(DIR_PATH_ABSOLUTE_PROJECT)) {
 		throw new Error(`env.DIR_PATH_ABSOLUTE_PROJECT:${DIR_PATH_ABSOLUTE_PROJECT} not an absolute path!`);
 	}
 
 	const appName = getAppName(DIR_PATH_ABSOLUTE_PROJECT);
 	// print(appName);
 	// print(LIBRARY_NAME);
+	if (!options.env) {
+		options.env = {};
+	}
 	options.env.APP_NAME = appName;
 	options.env.LIBRARY_NAME = LIBRARY_NAME;
 	print(options, { maxItems: Infinity });
