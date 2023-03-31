3ff979a5d85e1ca9777d3426e5615c7aa0517a57
Christian Westgaard <comlock@gmail.com>
2023-03-31 11:40:34 +0200

    Fix #195 Combine tasks into one

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,9 +1,5 @@
 export interface Environment {
-  BUILD_ENV? :string
-  CHUNK_DIRS? :string
-  ENTRY_DIRS? :string
-  ENTRY_EXT? :string
-  DIR_PATH_ABSOLUTE_PROJECT? :string
-  NASHORNPOLYFILLS_SOURCE? :string
-  VERBOSE? :string //Command line params can only be string (not boolean)
+	CHUNK_DIRS? :string
+	ENTRY_DIRS? :string
+	ENTRY_EXT? :string
 }
---
+++ src/util/logLevelFromGradle.ts
@@ -0,0 +1,40 @@
+export enum GRADLE_LOG_LEVEL {
+	'QUIET' = 'QUIET', // 0
+	'ERROR' = 'ERROR', // 1
+	'WARN' = 'WARN', // 2
+	'LIFECYCLE' = 'LIFECYCLE', // 3
+	'INFO' = 'INFO', // 4
+	'DEBUG' = 'DEBUG', // 5
+}
+
+export enum WEBPACK_STATS_LOG_LEVEL {
+	// https://webpack.js.org/configuration/stats/#statslogging
+	// https://webpack.js.org/configuration/other-options/#level
+	'NONE' = 'none', // disable logging
+	'ERROR' = 'error', // errors only
+	'WARN' = 'warn', // errors and warnings only
+	'INFO' = 'info', // errors, warnings, and info messages
+	'LOG' = 'log', // errors, warnings, info messages, log messages, groups, clears. Collapsed groups are displayed in a collapsed state
+	'VERBOSE' = 'verbose', // log everything except debug and trace
+	// https://webpack.js.org/configuration/stats/#statsloggingdebug
+	// https://webpack.js.org/configuration/stats/#statsloggingtrace
+}
+
+
+export default function logLevelFromGradle(gradleLogLevel = GRADLE_LOG_LEVEL.LIFECYCLE) {
+	switch (gradleLogLevel) {
+		case GRADLE_LOG_LEVEL.QUIET:
+			return WEBPACK_STATS_LOG_LEVEL.NONE; // disable logging
+		case GRADLE_LOG_LEVEL.ERROR: return WEBPACK_STATS_LOG_LEVEL.ERROR; // errors only
+		case GRADLE_LOG_LEVEL.WARN: return WEBPACK_STATS_LOG_LEVEL.WARN; // errors and warnings only
+		case GRADLE_LOG_LEVEL.LIFECYCLE: // default in gradle
+			return WEBPACK_STATS_LOG_LEVEL.INFO; // errors, warnings, and info messages
+		case GRADLE_LOG_LEVEL.INFO:
+			return WEBPACK_STATS_LOG_LEVEL.LOG; // errors, warnings, info messages, log messages, groups, clears. Collapsed groups are displayed in a collapsed state
+		case GRADLE_LOG_LEVEL.DEBUG:
+			return WEBPACK_STATS_LOG_LEVEL.VERBOSE; // log everything except debug and trace
+		default:
+			console.warn(`Unknown log level '${gradleLogLevel}' falling back to default log level 'info'`)
+			return WEBPACK_STATS_LOG_LEVEL.INFO;
+	}
+}
