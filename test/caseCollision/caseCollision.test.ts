6c64f47f7e407f99d29f806c0efc767386a03cb5
Christian Westgaard <comlock@gmail.com>
2023-08-18 12:33:35 +0200

    Turn of colors while running test

--- test/caseCollision/caseCollision.test.ts
+++ test/caseCollision/caseCollision.test.ts
@@ -10,11 +10,11 @@ import { execSync } from 'node:child_process';
 describe('components', () => {
 	it('throws when an entry collides with a chunkdir (case insensitive)', () => {
 		try {
-			execSync('cross-env-shell NODE_ENV=development R4X_APP_NAME=com.enonic.app.whatever R4X_BUILD_LOG_LEVEL=INFO R4X_DIR_PATH_ABSOLUTE_PROJECT=$INIT_CWD/test/caseCollision yarn webpack:components', {
+			execSync('cross-env-shell NO_COLOR=true NODE_ENV=development R4X_APP_NAME=com.enonic.app.whatever R4X_BUILD_LOG_LEVEL=INFO R4X_DIR_PATH_ABSOLUTE_PROJECT=$INIT_CWD/test/caseCollision TERM=vt220 yarn webpack:components', {
 				stdio: 'pipe'
 			});
 		} catch (e) {
-			expect(e.message.split(/\r?\n/)[1]).to.equal('[webpack-cli] \u001b[31mError: Entry name collision:ChUnKdIr!');
+			expect(e.message.split(/\r?\n/)[1]).to.equal('[webpack-cli] Error: Entry name collision:ChUnKdIr!');
 		}
 	});
 });
