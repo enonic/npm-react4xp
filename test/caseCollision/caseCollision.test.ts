77a5963f2fc37730ea2dfe68fa7ce8400b1c3f58
alansemenov <ase@enonic.com>
2025-03-07 10:16:33 +0100

    Removed usage of yarn in tests

--- test/caseCollision/caseCollision.test.ts
+++ test/caseCollision/caseCollision.test.ts
@@ -9,7 +9,7 @@ import { execSync } from 'node:child_process';
 describe('components', () => {
 	it('throws when an entry collides with a chunkdir (case insensitive)', () => {
 		try {
-			execSync('cross-env-shell NO_COLOR=true NODE_ENV=development R4X_APP_NAME=com.enonic.app.whatever R4X_BUILD_LOG_LEVEL=INFO R4X_DIR_PATH_ABSOLUTE_PROJECT=$INIT_CWD/test/caseCollision TERM=vt220 yarn webpack:components', {
+			execSync('cross-env-shell NO_COLOR=true NODE_ENV=development R4X_APP_NAME=com.enonic.app.whatever R4X_BUILD_LOG_LEVEL=INFO R4X_DIR_PATH_ABSOLUTE_PROJECT=$INIT_CWD/test/caseCollision TERM=vt220 npm run webpack:components', {
 				stdio: 'pipe'
 			});
 		} catch (e) {
